6cafabcb1cca97d1a6c867eebfcf71824402e84a
Ilia Bushmakin <im.bushmakin@gmail.com>
2025-05-26 16:37:20 +0400

    fix: fix matrix paddings

--- packages/padata/src/padata/utils/pad.py
+++ packages/padata/src/padata/utils/pad.py
@@ -1,55 +1,51 @@
+from collections.abc import Sequence
+from typing import Literal
+
 import torch
 
 
+def _get_pad(dims: Sequence[int], target_dims: Sequence[int], padding_side: Literal["right", "left"]) -> list[int]:
+    """Get padding values based on the dimensions and padding side."""
+    pad: list[int] = []
+    for dim, target in zip(reversed(dims), reversed(target_dims), strict=False):
+        if dim > target:
+            msg = "All dimensions must be less than or equal to the target dimensions."
+            raise ValueError(msg)
+        if padding_side == "right":
+            pad.extend((0, target - dim))
+        else:
+            pad.extend((target - dim, 0))
+    return pad
+
+
 def pad_matrix(
-    matrix: torch.Tensor | list[torch.Tensor], padding_value: float = 0, padding_side: str = "right"
+    matrix: torch.Tensor | list[torch.Tensor],
+    padding_value: float = 0,
+    padding_side: Literal["right", "left"] = "right",
 ) -> torch.Tensor:
     """Pad matrices to the same size with a specified value on the left or right side.
 
     Args:
-        matrix (torch.Tensor | list[torch.Tensor]): The matrix to pad.
+        matrix (torch.Tensor | list[torch.Tensor]): The 3d or 4d tensors to pad.
         padding_value (float, optional): The value to use for padding. Defaults to 0.
-        padding_side (str, optional): The side to pad. Can be "left" or "right". Defaults to "right".
+        padding_side (Literal["right", "left"], optional): The side to pad. Can be "left" or "right".
+            Defaults to "right".
 
     Returns:
         torch.Tensor: The padded matrix.
 
     """
     if not isinstance(matrix, list):
-        matrix = [matrix]
+        return matrix
 
-    max_size_1 = max(m.shape[1] for m in matrix)
-    max_size_2 = max(m.shape[2] for m in matrix)
-    if padding_side == "right":
-        return torch.concat(
-            [
-                torch.nn.functional.pad(
-                    m,
-                    (
-                        0,
-                        0,
-                        0,
-                        max_size_1 - m.shape[1],
-                        0,
-                        max_size_2 - m.shape[2],
-                    ),
-                    value=padding_value,
-                )
-                for m in matrix
-            ]
-        )
+    target_size = [max(m.shape[1] for m in matrix), max(m.shape[2] for m in matrix)]
+    if matrix[0].ndim > 3:  # noqa: PLR2004
+        target_size.append(matrix[0].shape[3])
     return torch.concat(
         [
             torch.nn.functional.pad(
                 m,
-                (
-                    0,
-                    0,
-                    max_size_1 - m.shape[1],
-                    0,
-                    max_size_2 - m.shape[2],
-                    0,
-                ),
+                _get_pad(m.shape[1:], target_size, padding_side),
                 value=padding_value,
             )
             for m in matrix
--- tests/padata/utils/test_pad.py
+++ tests/padata/utils/test_pad.py
@@ -5,16 +5,27 @@ from padata.utils.pad import pad_matrix
 
 
 @st.composite
-def random_tensor(draw):
+def random_3d_tensor(draw):
     """Generate a random tensor of shape n_dim 3 or 4."""
-    n_dim = draw(st.integers(min_value=3, max_value=4))
-    dims = draw(st.lists(st.integers(min_value=1, max_value=10), min_size=n_dim, max_size=n_dim))
+    dims = draw(st.lists(st.integers(min_value=1, max_value=10), min_size=3, max_size=3))
     return torch.randn(*dims)
 
 
-@given(st.lists(random_tensor(), min_size=1, max_size=10))
-def test_pad(matrices: list[torch.Tensor]):
+@st.composite
+def random_4d_tensor(draw):
+    """Generate a random tensor of shape n_dim 4."""
+    dims = draw(st.lists(st.integers(min_value=1, max_value=10), min_size=3, max_size=3))
+    return torch.randn(*dims, 4)
+
+
+@given(st.lists(random_3d_tensor(), min_size=1, max_size=10) | st.lists(random_4d_tensor(), min_size=1, max_size=10))
+def test_3d_tensors_pad(matrices: list[torch.Tensor]):
     """Test the pad_matrix function."""
     padded_matrix = pad_matrix(matrices, padding_value=0, padding_side="right")
-
-    assert padded_matrix.shape == (6, 7, 8)
+    size_1 = max(matrix.shape[1] for matrix in matrices)
+    size_2 = max(matrix.shape[2] for matrix in matrices)
+    batch_size = sum(matrix.shape[0] for matrix in matrices)
+    if matrices[0].ndim == 4:
+        assert padded_matrix.shape == (batch_size, size_1, size_2, matrices[0].shape[3])
+    else:
+        assert padded_matrix.shape == (batch_size, size_1, size_2)
